04a21467f223355152f5c359fd88e27e7639edab
sheepbelldoor <sheepbelldoor.dev@gmail.com>
2023-11-27 22:57:55 +0900

    modify: announcementList

--- app/src/main/java/com/se/blueboard/AnnouncementIn.java
+++ app/src/main/java/com/se/blueboard/AnnouncementIn.java
@@ -30,7 +30,7 @@ public class AnnouncementIn extends AppCompatActivity {
         //뒤로 가기 버튼 구현
         ImageButton backButton = findViewById(R.id.announcement_gotoBack);
         backButton.setOnClickListener(view -> {
-            Utils.gotoPage(getApplicationContext(), Announcement.class, null);
+            Utils.gotoPage(getApplicationContext(), AnnouncementPage.class, null);
         });
 
         controller.getAnnouncementData(getIntent().getExtras().getString("key"), new MyCallback() {
--- app/src/main/java/com/se/blueboard/AnnouncementPage.java
+++ app/src/main/java/com/se/blueboard/AnnouncementPage.java
@@ -1,22 +1,34 @@
 package com.se.blueboard;
 
+import static com.se.blueboard.LecturePage.currentLecture;
+import static com.se.blueboard.makeLecture.MakeLecturePageOne.makingLecture;
 import static utils.Utils.gotoPage;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 import adapter.AnnouncementAdapter;
 import model.Announcement;
+import model.User;
 import utils.FirebaseController;
+import utils.Utils;
 
 public class AnnouncementPage extends AppCompatActivity {
 
@@ -35,23 +47,50 @@ public class AnnouncementPage extends AppCompatActivity {
         adapter = new AnnouncementAdapter(announcements);
         listView = (ListView) findViewById(R.id.announcementList);
 
-        //adapter안에 공지사항 더미데이터 넣기(나중에 파베에서 가져오는 걸로 바꿔)
-        Date date = new Date(); // 일단 나중에 수정
-        Announcement announcement = new Announcement("공지사항1 test", "안녕하세요 저는 한국인 메롱메롱 아리가또", date, null, "공지사항1 test", null);
-        Announcement announcement2 = new Announcement("공지사항2 test", "두번째 테스트 글인데요 아따맘마 안녕하세요 잘지내요 조심해요", date, null, "공지사항2 test", null);
-        Announcement announcement3 = new Announcement("공지사항3 test", "세번째 테스트 글입니다 허허 토일 너 왜 앨범 안내니? 듣고 싶다 ~~", date, null, "공지사항3 test", null);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("announcements")
+                .whereEqualTo("lectureId", currentLecture.getId())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (DocumentSnapshot document: queryDocumentSnapshots.getDocuments()) {
+                            if (document.exists()) {
+                                announcements.add(document.toObject(Announcement.class));
+                                Log.d("successGetAnnouncementList", document.toObject(Announcement.class).toString());
 
-        controller.sendAnnouncementData(announcement);
-        controller.sendAnnouncementData(announcement2);
-        controller.sendAnnouncementData(announcement3);
+                                // 관리자 검색 기능
+                                SearchView searchView = (SearchView) findViewById(R.id.announcement_search);
+                                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                                    @Override
+                                    public boolean onQueryTextSubmit(String s) {
+                                        return false;
+                                    }
+                                    @Override
+                                    public boolean onQueryTextChange(String s) {
+                                        ArrayList<Announcement> filteredAnnouncementList = new ArrayList<>();
+                                        for (int i = 0; i < announcements.size(); i++) {
+                                            String announcementTitle = announcements.get(i).getTitle();
 
+                                            if (announcementTitle.toLowerCase().contains(s.toLowerCase()))
+                                                filteredAnnouncementList.add(announcements.get(i));
+                                        }
 
-        announcements.add(announcement);
-        announcements.add(announcement2);
-        announcements.add(announcement3);
+                                        AnnouncementAdapter announcementAdapter = new AnnouncementAdapter(filteredAnnouncementList);
+                                        listView.setAdapter(announcementAdapter);
 
+                                        return false;
+                                    }
+                                });
+                                // Set adapter
+                                listView.setAdapter(adapter);
+                            }
+                            else
+                                Log.d("ManagerList", "No such Document");
+                        }
+                    }
+                });
         listView.setAdapter(adapter);
-//        adapter.notifyDataSetChanged();
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
