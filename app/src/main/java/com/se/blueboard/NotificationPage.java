aab281739824b46d8db9c9ead05f378bf2577d1c
sj1226m <sj1226m@naver.com>
2023-11-29 15:13:16 +0900

    notification

--- app/src/main/java/com/se/blueboard/NotificationPage.java
+++ app/src/main/java/com/se/blueboard/NotificationPage.java
@@ -1,19 +1,29 @@
 package com.se.blueboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
 import adapter.NotificationAdapter;
 import model.Alarm;
+import model.Announcement;
 import utils.FirebaseController;
+
+import static com.se.blueboard.HomePage.currentUser;
 
 public class NotificationPage extends AppCompatActivity {
 
@@ -32,8 +42,23 @@ public class NotificationPage extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.notificationList);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("alarms")
+                .whereIn("id", currentUser.getCourses())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (DocumentSnapshot document: queryDocumentSnapshots.getDocuments()){
+                            if(document.exists()){
+                                alarms.add(document.toObject(Alarm.class));
+                                Log.d("successGetAlarmList", document.toObject(Alarm.class).toString());
 
-
+                                listView.setAdapter(adapter);
+                            }
+                        }
+                    }
+                });
+        listView.setAdapter(adapter);
 
     }
 
