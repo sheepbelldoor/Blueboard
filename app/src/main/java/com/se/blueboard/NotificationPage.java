56024b469c0e9c418cec4207cfa722aaedf57ea1
sj1226m <sj1226m@naver.com>
2023-11-29 14:47:38 +0900

    sendAlarm

--- app/src/main/java/com/se/blueboard/NotificationPage.java
+++ app/src/main/java/com/se/blueboard/NotificationPage.java
@@ -1,17 +1,39 @@
 package com.se.blueboard;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+import adapter.NotificationAdapter;
+import model.Alarm;
+import utils.FirebaseController;
+
 public class NotificationPage extends AppCompatActivity {
+
+    FirebaseController controller = new FirebaseController();
+
+    public ArrayList<Alarm> alarms = new ArrayList<>();
+
+    private ListView listView;
+    private NotificationAdapter adapter;
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification);
 
-        //사용자의 경우 과제 생성 알림 -> 제출 버튼, 그 외 모두 확인 버튼
+        adapter = new NotificationAdapter(alarms);
+        listView = (ListView) findViewById(R.id.notificationList);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
 
     }
 
